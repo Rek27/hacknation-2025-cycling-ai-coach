44b2c4fefee81a082ab573134311487dd09fdb2e
Savo <savo.simeunovic@gmail.com>
2025-08-08 21:30:10 +0200

    feat: docker and python setup

---
+++ python_backend/main.py
@@ -0,0 +1,9 @@
+import uvicorn
+from dotenv import load_dotenv
+from src.app import app
+
+load_dotenv(override=True)
+
+if __name__ == "__main__":
+    app = app()
+    uvicorn.run(app, host="0.0.0.0", port=8001)
---
+++ python_backend/src/app.py
@@ -0,0 +1,10 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from src.api.routers import health
+
+
+def app() -> FastAPI:
+    project = FastAPI(title="Temp", version="1.0.0")
+    project.include_router(health.router)
+
+    return project
